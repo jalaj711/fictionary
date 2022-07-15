78e2edd46248305482a5afd5a97c27e17838961c
Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>
2022-07-15 14:08:43 +0530

    register models in admin site
    Signed-off-by: Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import User, Question, Clues, AccessTokens
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Question)
+admin.site.register(Clues)
+admin.site.register(AccessTokens)
