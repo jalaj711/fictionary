95c2a6f7a1564f32d6ea478ebbfbd04dbcc5fc39
Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>
2022-07-18 13:26:35 +0530

    add question, answer, clue and leaderboard endpoints
    Signed-off-by: Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>

--- api/urls.py
+++ api/urls.py
@@ -6,4 +6,8 @@ from . import views
 urlpatterns = [
     path('login/', csrf_exempt(views.login.as_view()), name='login'),
     path('register/', csrf_exempt(views.register.as_view()), name='register'),
+    path('question/', csrf_exempt(views.question.as_view()), name='question'),
+    path('answer/', csrf_exempt(views.answer.as_view()), name='answer'),
+    path('clue/', csrf_exempt(views.clue.as_view()), name='clue'),
+    path('leaderboard/', csrf_exempt(views.leaderboard.as_view()), name='leaderboard')
 ]
--- api/views.py
+++ api/views.py
@@ -1,5 +1,7 @@
+from datetime import datetime
 import json
 from django.db.utils import IntegrityError
+from django.http import JsonResponse
 from .serializers import *
 from .models import User, Question, Clues
 from rest_framework.decorators import permission_classes, APIView
@@ -30,7 +32,7 @@ class register(generics.GenericAPIView):
                     password=request.data.get("password")
                 )
             except:
-                return Response("Username already used!!", status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+                return Response("Username already used!!", status=status.HTTP_400_INTERNAL_SERVER_ERROR)
             return Response(
                 {
                     "token": AuthToken.objects.create(user)[1],
@@ -68,3 +70,101 @@ class login(generics.GenericAPIView):
             return Response(
                 "Wrong Credentials! Please try again.", status=status.HTTP_403_FORBIDDEN
             )
+
+
+@permission_classes(
+    [IsAuthenticated]
+)
+class question(generics.GenericAPIView):
+    def get(self, request):
+        cround = request.user.current_round
+        try:
+            question = Question.objects.get(round=cround)
+            try:
+                media = question.media.url
+            except ValueError:
+                media = ''
+            return JsonResponse({
+                'text': question.text,
+                'round': question.round,
+                'media': media
+            })
+        except IntegrityError:
+            return JsonResponse({
+                'message': 'Question not found'
+            }, status=status.HTTP_404_NOT_FOUND)
+
+@permission_classes(
+    [IsAuthenticated]
+)
+class clue(generics.GenericAPIView):
+    def get(self, request):
+        cround = request.user.current_round
+        try:
+            question = Question.objects.get(round=cround)
+            clues = Clues.objects.filter(question=question)
+            clues = [clue.content for clue in clues]
+            return JsonResponse({
+                'clues': clues,
+            })
+        except IntegrityError:
+            return JsonResponse({
+                'message': 'Question not found'
+            }, status=status.HTTP_404_NOT_FOUND)
+
+@permission_classes(
+    [IsAuthenticated]
+)
+class answer(generics.GenericAPIView):
+    def post(self, request):
+        cround = request.user.current_round
+
+        if 'answer' not in request.data.keys():
+            return JsonResponse({'message': 'Empty answer not accepted'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        answer = request.data.get('answer').lower().strip()
+
+        try:
+            question = Question.objects.get(round=cround)
+            if question.answer == answer:
+                request.user.current_round = cround + 1
+                request.user.points += 10
+                request.user.time = datetime.now()
+                request.user.save()
+                return JsonResponse({
+                    'success': True
+                })
+
+            return JsonResponse({
+                'success': False
+            })
+        except IntegrityError:
+            return JsonResponse({
+                'message': 'Question not found'
+            }, status=status.HTTP_404_NOT_FOUND)
+
+@permission_classes(
+    [IsAuthenticated]
+)
+class leaderboard(generics.GenericAPIView):
+    def get(self, request):
+        #try:
+        leaderboard = list(User.objects.filter().order_by('-points'))
+
+        # Tie breaker in case of same points
+        for i in range(len(leaderboard)):
+            for j in range(i, len(leaderboard)):
+                if leaderboard[i].points == leaderboard[j].points:
+                    if leaderboard[i].time > leaderboard[j].time:
+                        leaderboard[i], leaderboard[j] = leaderboard[j], leaderboard[i]
+        
+        leaderboard = [{'name': user.username, 'points': user.points} for user in leaderboard]
+
+        return JsonResponse({
+            'leaderboard': leaderboard
+            })
+        # except:
+        #     return JsonResponse({
+        #         'message': 'Server failed to process the request'
+        #     }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
