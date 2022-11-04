4770d6c0c91c75223a3425401ffcd1bfb63d8153
Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>
2022-11-05 02:51:52 +0530

    add feature to disable checking for end time on endpoints with time restrictions
    Signed-off-by: Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>

--- api/urls.py
+++ api/urls.py
@@ -7,19 +7,23 @@ from django.http import JsonResponse
 from . import views
 
 
-def _available_when_live(view_function, *args, **kwargs):
+def _available_when_live(view_function, check_end, *args, **kwargs):
     meta = Meta.objects.filter()[0]
     now = timezone.now()
-    if now > meta.start_time and now < meta.end_time:
-        return view_function(*args, **kwargs)
+    if now > meta.start_time:
+        if not check_end:
+            return view_function(*args, **kwargs)
+        elif now < meta.end_time:
+            return view_function(*args, **kwargs)
+
     return JsonResponse({
         'game_not_live': True,
         'message': "The game is not live"
     }, status=400)
 
 
-def available_when_live(view_function):
-    return lambda *args, **kwargs: _available_when_live(view_function, *args, **kwargs)
+def available_when_live(view_function, check_end=True):
+    return lambda *args, **kwargs: _available_when_live(view_function, check_end, *args, **kwargs)
 
 
 urlpatterns = [
@@ -31,7 +35,7 @@ urlpatterns = [
     path('clue/', csrf_exempt(available_when_live(views.clue.as_view())), name='clue'),
     path('check-clue-available/', csrf_exempt(available_when_live(
         views.checkClueAvailability.as_view())), name='clueAvailability'),
-    path('leaderboard/', csrf_exempt(views.leaderboard.as_view()), name='leaderboard'),
+    path('leaderboard/', csrf_exempt(available_when_live(views.leaderboard.as_view(), check_end=False)), name='leaderboard'),
     path('accounts/get-social-token/',
          csrf_exempt(views.sociallogin_get_token), name='social_token_generator')
 ]
